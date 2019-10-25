bae2fafad79f0fab17f545e4493627d6b9e2d62e
miyagi-sho <mygsho@outlook.com>
2019-10-25 22:24:07 +0900

    [routes] インデント修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,24 +9,24 @@ class UserController extends Controller
 {
     public function index(User $user)
     {
-        return view('users/index',[
+        return view('users/index', [
             'user_id' => $user->id,
         ]);
     }
 
     public function showEditForm(User $user)
     {
-        return view('users/edit',[
+        return view('users/edit', [
             'user' => $user,
         ]);
     }
 
-    public function edit(User $user,EditUser $request)
+    public function edit(User $user, EditUser $request)
     {
         $user->name = $request->name;
         $user->save();
 
-        return redirect()->route('users.index',[
+        return redirect()->route('users.index', [
             'id' => $user->id,
         ]);
     }
