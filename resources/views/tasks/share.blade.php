045e6368d395a76dbc93c616c35ce68314c11104
miyagi-sho <mygsho@outlook.com>
2019-10-31 15:23:28 +0900

    [resources] シェアURL表示画面の追加

---
+++ resources/views/tasks/share.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col col-md-offset-3 col-md-6">
+                <nav class="panel panel-default">
+                    <div class="panel-heading">公開されているURL</div>
+                    <div class="panel-body">
+                        <p>URL</p>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+@endsection
