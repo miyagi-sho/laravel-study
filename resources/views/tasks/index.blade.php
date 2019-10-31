101b5c8a49f4f144f689d3a67b08532cf8e9d72c
miyagi-sho <mygsho@outlook.com>
2019-10-31 15:16:50 +0900

    [resources] シェル作成リンクの追加

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,15 +7,15 @@
                <nav class="panel panel-default">
                    <div class="panel-heading">フォルダ</div>
                    <div class="panel-body">
-                       <a 
-                       href="{{ route('folders.create') }}" 
+                       <a
+                       href="{{ route('folders.create') }}"
                        class="btn btn-default btn-block">
                            フォルダを追加する
                        </a>
                    </div>
                    <div class="list-group">
                        @foreach($folders as $folder)
-                         <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" 
+                         <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
                          class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}"
                          >
                              {{ $folder->title }}
@@ -25,7 +25,7 @@
                </nav>
            </div>
            <div class="column col-md-8">
-               
+
                <!-- 以下にタスクが表示される　-->
                <div class="panel panel-default">
                    <div class="panel-heading">タスク</div>
@@ -54,9 +54,15 @@
                                   </td>
                                   <td>{{ $task->formatted_due_date }}</td>
                                   <td>
-                                    <a href="{{ route('tasks.edit', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">
-                                    編集
-                                    </a>
+                                      <a href="#" id="share">シェア</a>
+                                      <form id="share-form" action="{{ route('tasks.share', ['id' => $task->folder_id, 'task_id' => $task->id]) }}" method="POST" style="display:none;">
+                                          @csrf
+                                      </form>
+                                  </td>
+                                  <td>
+                                    　<a href="{{ route('tasks.edit', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">
+                                    　　　 編集
+                                    　</a>
                                   </td>
                               </tr>
                            @endforeach
@@ -65,5 +71,11 @@
                </div>
            </div>
        </div>
-   </div> 
+   </div>
+  <script>
+      document.getElementById('share').addEventListener('click', function(event){
+          event.preventDefault();
+          document.getElementById('share-form').submit();
+      });
+  </script>
 @endsection
