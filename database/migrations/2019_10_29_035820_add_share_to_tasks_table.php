57176a59eb13b0102017351d9317c6394ff8ef98
miyagi-sho <mygsho@outlook.com>
2019-11-04 01:34:09 +0900

    [database] dropColumn　追加

--- database/migrations/2019_10_29_035820_add_share_to_tasks_table.php
+++ database/migrations/2019_10_29_035820_add_share_to_tasks_table.php
@@ -26,7 +26,7 @@ class AddShareToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->dropColumn('share');
         });
     }
 }
