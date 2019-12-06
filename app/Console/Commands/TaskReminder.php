8d7c1e15bd1d5ea1cf0fc0c02516bf390a2c1399
miyagi-sho <mygsho@outlook.com>
2019-12-06 23:31:52 +0900

    whereInを使った処理に変更

--- app/Console/Commands/TaskReminder.php
+++ app/Console/Commands/TaskReminder.php
@@ -51,51 +51,33 @@ class TaskReminder extends Command
             ->orWhere('status', '2')
             ->get();
 
-        //上記タスクのフォルダを探し出す。
-        $folders = 0;
+        //未完了タスクを含むフォルダを探し出す。
+        $folders_id = [];
         foreach ($tasks as $task) {
-            if ($folders === 0) {
-                $folders = Folder::where('id', $task->folder_id)->get();
-            } else {
-                $folder = Folder::where('id', $task->folder_id)->first();
-                //$folders内に同じfolderがなければ、$foldersに追加する。
-                $folders_id = [];
-                for($i = 0; $i < count($folders); $i++){
-                    $id = $folders[$i]->id;
-                    $folders_id[] = $id;
-                }
-                if (in_array($folder->id, $folders_id) === false){
-                    $folders[] = $folder;
-                }
-            }
+            $id = $task->folder_id;
+            $folders_id[] = $id;
         }
+        $folders_id = array_unique($folders_id);
+        $folders = Folder::whereIn('id', $folders_id)->get();
 
-        //そのフォルダのuser_idのと一致するuserを探し出す。
-        $users = 0;
+        //上記フォルダのユーザーを探し出す。
+        $users_id = [];
         foreach ($folders as $folder) {
-            if ($users === 0) {
-                $users = User::where('id', $folder->user_id)->get();
-            } else {
-                $user = User::where('id', $folder->user_id)->first();
-                //$users内に同じuserがいなければ、$usersに追加する。
-                $users_id = [];
-                for($i = 0; $i < count($users); $i++){
-                    $id = $users[$i]->id;
-                    $users_id[] = $id;
-                }
-                if (in_array($user->id, $users_id) === false){
-                    $users[] = $user;
-                }
-            }
+            $id = $folder->user_id;
+            $users_id[] = $id;
         }
+        $users_id = array_unique($users_id);
+        $users = User::whereIn('id', $users_id)->get();
 
         //ユーザーごとに情報を渡し、メールを送信
         foreach ($users as $user) {
             //未完了タスクを持つフォルダをuserごとに処理する
             $reminder_folders = [];
+            $reminder_folders_id = [];
             foreach ($folders as $folder) {
                 if ($folder->user_id === $user->id) {
                     $reminder_folders[] = $folder;
+                    $reminder_folders_id[] = $folder->id;
                 }
             }
 
