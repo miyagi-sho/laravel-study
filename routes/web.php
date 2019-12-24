f3c35d0881954c8bf9d54819fccf20b9310a2840
miyagi-sho <mygsho@outlook.com>
2019-12-24 23:23:43 +0900

    不要なルートの削除

--- routes/web.php
+++ routes/web.php
@@ -40,8 +40,4 @@ Route::group(['middleware' => 'auth'], function(){
 
 Route::get('/folders/{folder}/tasks/{task}/share/{share}', 'TaskController@publicTask')->name('tasks.public');
 
-Route::get('search', function () {
-    return App\Task::search(\request('q'))->paginate();
-});
-
 Auth::routes();
